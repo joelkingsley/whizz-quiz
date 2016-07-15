e4fc99773301858166b623fb6a0c1af9bfa09051
joelkingsley <joelkingsley.r@gmail.com>
2016-07-16 01:44:54 +0530

    Update and rename main2.h to user.h

--- user.h
+++ user.h
@@ -6,7 +6,7 @@ class user
 	int score;//Score i.e. total correct answers
 	int exp;//Experience i.e. total number of questions attempted in lifetime
 	char level[20];//Level i.e. calculated by (score/exp)*100. starts from Lv1
-    void loginuser();//Logins in user
+    	void loginuser();//Logins in user
 	int checkname(char *);
 	void loginme(char *);
 	void registeruser();
